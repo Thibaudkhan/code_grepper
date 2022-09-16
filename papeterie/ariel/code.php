01dc683ab9d0cc749bfd49a40b08acf9d35a14eb
Thibaudkhan <43219426+Thibaudkhan@users.noreply.github.com>
2022-09-16 15:47:08 +0200

    Add working request php

--- papeterie/ariel/code.php
+++ papeterie/ariel/code.php
@@ -1,5 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Http;
+/*
+* You must have Guzzle, just run the next commands in your project (you need composer)
+* composer require guzzlehttp/guzzle
+* composer install
+*/
 
-$response = Http::get("https://ariel.pfregistres.com/apiUnlocked/all/{siren}");
+$client = new \GuzzleHttp\Client();
+$response = $client->get('https://ariel.pfregistres.com/apiUnlocked/all/{siren}');
+
+echo $response->getBody();
